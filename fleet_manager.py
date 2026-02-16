e676a4c6bdfbea5ba793c7813c985db768d1432f
Adan-Khan1 <ak139@kent.ac.uk>
2026-02-16 00:38:14 +0000

    FEATURE 1: init_database()

---
+++ fleet_manager.py
@@ -0,0 +1,8 @@
+# Feature 1: init_database()
+
+def init_database(): 
+    names = ["Jean-Luc Picard", "William Riker", "Data", "Geordi La Forge", "Beverly Crusher"]
+    ranks = ["Captain", "Commander", "Lt. Commander", "Lt. Commander", "Commander"]
+    divs = ["Command", "Command", "Operations", "Engineering", "Sciences"]
+    ids = ["1701", "1702", "1703", "1704", "1705"]
+    return names, ranks, divs, ids
